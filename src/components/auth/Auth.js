c4db60044404a645a01d86b9a3c421bcadf5a189
Cameron <orrca14@gmail.com>
2020-04-11 00:48:12 -0400

    Finished the layout of the frontend, did context for the login/register

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import logo from './safety-guy.png';
 import check from './checkmark.png';
-import { Link, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+
 
 const Auth = () => {
+
+    const authContext = useContext(AuthContext);
+
+    const { login, isAuthenticated } = authContext;
 
     const [user, setUser] = useState({
         email: '',
@@ -19,10 +24,10 @@ const Auth = () => {
         if (email === '' || password === '') {
             console.log('Please fill in all fields');
         } else {
-            // login({
-            //     email,
-            //     password
-            // });
+            login({
+                email,
+                password
+            });
         }
     }
 
@@ -30,7 +35,8 @@ const Auth = () => {
         newEmail: '',
         name: '',
         newPassword: '',
-        confirmPassword: ''
+        confirmPassword: '',
+        model: ''
     });
 
     const { newEmail, name, newPassword, confirmPassword } = newUser;
@@ -47,175 +53,193 @@ const Auth = () => {
             newEmail === '' ||
             name === '' ||
             newPassword === '' ||
-            confirmPassword === ''
+            confirmPassword === '' ||
+            model === ''
         ) {
             console.log('Please fill out all fields.');
         }else {
             console.log(newEmail, name, newPassword);
         }
     }
-
-    return(
-        <div className='grid-hor'>
-            <div className='card-nh-auth-log'>
-                <div className='grid-hor'>
-                    <div style={{ textAlign: 'left' }}>
-                        <h1 className='text-primary' style={{ fontSize: '3.5rem', font: 'Candal', fontWeight: 'bolder' }}>Login</h1>
-                    </div>
-                    <div>
-                    </div>
-                </div>
-                <div className='grid-hor'>
-                    <div style={{ maxWidth: '20vw', textAlign: 'center' }}>
-                        <img src={logo} style={{ maxWidth: '70%' }} alt="Logo"/>
-                    </div>
-                    <div style={{ verticalAlign: 'center' }}>
-                        <div style={{ textAlign: 'right' }}>
-                            <form onSubmit={onSubmitLog}>
-                                <div>
-                                <label
-                                    htmlFor='email'
-                                    className='login-label'
-                                >
-                                    Email
-                                </label>
-                                <input
-                                    type='email'
-                                    name='email'
-                                    value={email}
-                                    onChange={onChangeLog}
-                                    required
-                                    style={{ marginLeft: 'auto', width: '14vw' }}
-                                />
-                                </div>
-                                <div>
-                                <label
-                                    htmlFor='password'
-                                    className='login-label'
-                                >
-                                    Password
-                                </label>
-                                <input
-                                    type='password'
-                                    name='password'
-                                    value={password}
-                                    onChange={onChangeLog}
-                                    required
-                                    style={{ marginLeft: 'auto', width: '14vw' }}
-                                />
-                                </div>
-                                <div>
-                                    {/* eslint-disable-next-line */}
-                                    <a href='#'>
-                                        <input
-                                        type='submit'
-                                        value='Login'
-                                        className='btn btn-primary btn-block'
-                                        style={{ marginLeft: 'auto', width: '5vw' }}
-                                        />
-                                    </a>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <div className='card-nh-auth'>
+    if(!isAuthenticated){
+        return(
+            <div className='grid-hor'>
+                <div className='card-nh-auth-log'>
                     <div className='grid-hor'>
                         <div style={{ textAlign: 'left' }}>
-                            <h1 className='text-primary' style={{ fontSize: '3.5rem' }}>Register</h1>
+                            <h1 className='text-primary' style={{ fontSize: '3.5rem', font: 'Candal', fontWeight: 'bolder' }}>Login</h1>
                         </div>
-                        <div style={{ textAlign: 'right' }}>
-                            <label
-                                htmlFor='email'
-                                className='login-label'
-                                style={{ marginLeft: 'auto', textAlign: 'right' }}
-                            >
-                                Email:
-                            </label>
-                            <input
-                                type='email'
-                                name='email'
-                                value={newEmail}
-                                placeholder='john@gmail.com'
-                                onChange={onChangeReg}
-                                required
-                                style={{ marginLeft: 'auto', textAlign: 'right' }}
-                            />
+                        <div>
                         </div>
                     </div>
                     <div className='grid-hor'>
-                        <div>
-                            <img src={check} style={{ maxWidth: '70%' }} alt="Logo"/>
+                        <div style={{ maxWidth: '20vw', textAlign: 'center' }}>
+                            <img src={logo} style={{ maxWidth: '70%' }} alt="Logo"/>
                         </div>
                         <div style={{ verticalAlign: 'center' }}>
                             <div style={{ textAlign: 'right' }}>
-                                <form onSubmit={onSubmitReg}>
-                                    
-                                    <label
-                                        htmlFor='name'
-                                        className='login-label'
-                                        style={{ marginLeft: 'auto' }}
-                                    >
-                                        Name:
-                                    </label>
-                                    <input
-                                        type='text'
-                                        name='name'
-                                        value={name}
-                                        placeholder='Johnny Appleseed'
-                                        onChange={onChangeReg}
-                                        required
-                                        style={{ marginLeft: 'auto', textAlign: 'right' }}
-                                    />
+                                <form onSubmit={onSubmitLog}>
+                                    <div>
                                     <label
                                         htmlFor='email'
                                         className='login-label'
-                                        style={{ marginLeft: 'auto' }}
                                     >
-                                        Password:
+                                        Email
+                                    </label>
+                                    <input
+                                        type='email'
+                                        name='email'
+                                        value={email}
+                                        onChange={onChangeLog}
+                                        required
+                                        style={{ marginLeft: 'auto', width: '14vw' }}
+                                    />
+                                    </div>
+                                    <div>
+                                    <label
+                                        htmlFor='password'
+                                        className='login-label'
+                                    >
+                                        Password
                                     </label>
                                     <input
                                         type='password'
                                         name='password'
-                                        value={newPassword}
-                                        onChange={onChangeReg}
+                                        value={password}
+                                        onChange={onChangeLog}
                                         required
-                                        style={{ marginLeft: 'auto', textAlign: 'right' }}
+                                        style={{ marginLeft: 'auto', width: '14vw' }}
                                     />
-                                    <label
-                                        htmlFor='email'
-                                        className='login-label'
-                                        style={{ marginLeft: 'auto' }}
-                                    >
-                                        Confirm Password:
-                                    </label>
-                                    <input
-                                        type='password'
-                                        name='confirmPassword'
-                                        value={confirmPassword}
-                                        onChange={onChangeReg}
-                                        required
-                                        style={{ marginLeft: 'auto', textAlign: 'right' }}
-                                    />
-                                    <a href='#'>
-                                        <input
-                                        type='submit'
-                                        value='Register'
-                                        className='btn btn-block btn-primary'
-                                        style={{ marginLeft: 'auto', width: '5vw' }}
-                                        />
-                                    </a>
+                                    </div>
+                                    <div>
+                                        {/* eslint-disable-next-line */}
+                                        <a href='#'>
+                                            <input
+                                            type='submit'
+                                            value='Login'
+                                            className='btn btn-primary btn-block'
+                                            style={{ marginLeft: 'auto', width: '5vw' }}
+                                            />
+                                        </a>
+                                    </div>
                                 </form>
                             </div>
                         </div>
                     </div>
                 </div>
-                
+                <div>
+                    <div className='card-nh-auth'>
+                        <div className='grid-hor'>
+                            <div style={{ textAlign: 'left' }}>
+                                <h1 className='text-primary' style={{ fontSize: '3.5rem' }}>Register</h1>
+                            </div>
+                            <div style={{ textAlign: 'right' }}>
+                                <label
+                                    htmlFor='email'
+                                    className='login-label'
+                                    style={{ marginLeft: 'auto', textAlign: 'right' }}
+                                >
+                                    Email:
+                                </label>
+                                <input
+                                    type='email'
+                                    name='email'
+                                    value={newEmail}
+                                    placeholder='john@gmail.com'
+                                    onChange={onChangeReg}
+                                    required
+                                    style={{ marginLeft: 'auto', textAlign: 'right' }}
+                                />
+                            </div>
+                        </div>
+                        <div className='grid-hor'>
+                            <div>
+                                <img src={check} style={{ maxWidth: '70%' }} alt="Logo"/>
+                            </div>
+                            <div style={{ verticalAlign: 'center' }}>
+                                <div style={{ textAlign: 'right' }}>
+                                    <form onSubmit={onSubmitReg}>
+                                        
+                                        <label
+                                            htmlFor='name'
+                                            className='login-label'
+                                            style={{ marginLeft: 'auto' }}
+                                        >
+                                            Name:
+                                        </label>
+                                        <input
+                                            type='text'
+                                            name='name'
+                                            value={name}
+                                            placeholder='Johnny Appleseed'
+                                            onChange={onChangeReg}
+                                            required
+                                            style={{ marginLeft: 'auto', textAlign: 'right' }}
+                                        />
+                                        <label
+                                            htmlFor='email'
+                                            className='login-label'
+                                            style={{ marginLeft: 'auto' }}
+                                        >
+                                            Password:
+                                        </label>
+                                        <input
+                                            type='password'
+                                            name='password'
+                                            value={newPassword}
+                                            onChange={onChangeReg}
+                                            required
+                                            style={{ marginLeft: 'auto', textAlign: 'right' }}
+                                        />
+                                        <label
+                                            htmlFor='email'
+                                            className='login-label'
+                                            style={{ marginLeft: 'auto' }}
+                                        >
+                                            Confirm Password:
+                                        </label>
+                                        <input
+                                            type='password'
+                                            name='confirmPassword'
+                                            value={confirmPassword}
+                                            onChange={onChangeReg}
+                                            required
+                                            style={{ marginLeft: 'auto', textAlign: 'right' }}
+                                        />
+                                        <label
+                                            htmlFor='email'
+                                            className='login-label'
+                                            style={{ marginLeft: 'auto' }}
+                                        >
+                                            Car Model
+                                        </label>
+                                        <input
+                                            type='email'
+                                            name='model'
+                                            value={model}
+                                            onChange={onChangeReg}
+                                            required
+                                            style={{ marginLeft: 'auto', textAlign: 'right' }}
+                                        />
+                                        <a href='#'>
+                                            <input
+                                            type='submit'
+                                            value='Register'
+                                            className='btn btn-block btn-primary'
+                                            style={{ marginLeft: 'auto', width: '5vw' }}
+                                            />
+                                        </a>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
-        </div>
-    );
+        );
+    } else
+        return <Redirect to='/' />;
 }
 
 export default Auth;
--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,11 +1,152 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { geolocated } from 'react-geolocated';
+import { ReactDOM } from 'react-dom';
 
-const Main = () => {
+const Main = ({ carModel, hazardRating, path, google, coords }) => {
+
+    const { latitude, longitude } = coords;
+
+    const [pos, setCoordinates] = useState ({
+        lat: null,
+        long: null
+    })
+
+    const { lat, long } = pos;
+
+    const mapStyles = {
+        width: '100%',
+        height: '100%'
+    };
+
+    const onChange = e => 
+        setCoordinates({ ...pos, [e.target.name]: e.target.value });
+
+
+    const [marker, setMarker] = useState ({
+        curr: {
+            lat: latitude,
+            long: longitude
+        },
+        dest: {
+            lat: latitude,
+            long: longitude
+        }
+    });
+
+    const { curr, dest } = marker;
+
+    const onSubmit = e => 
+        setMarker({ ...marker, [e.target.name]: {lat: lat, long: long} })
+
     return(
-        <div>
-            
+        <div className='grid-ver'>
+            <div className='grid-hor-f' style={{ paddingBottom : '2rem' }}>
+                <div className='grid-ver-t'>
+                    <div style={{ verticalAlign: 'top' }}>
+                        <h1 className='text-primary' style={{ fontSize: '3rem', fontWeight: 'bolder' }}>Car Model</h1>
+                        <body style={{ fontSize: '2rem' }}>{carModel}</body>
+                    </div>
+                    <div />
+                </div>
+                <div className='grid-hor-f-c'>
+                    <div className='card-small' style={{ textAlign: 'right', paddingRight: '1rem' }}>
+                        <label
+                            htmlFor='name'
+                            className='login-label'
+                        >
+                            Latitude
+                        </label>
+                        <input
+                            type='text'
+                            name='lat'
+                            value={lat}
+                            placeholder='e.g., 47.444'
+                            onChange={onChange}
+                            required
+                            style={{ marginLeft: 'auto', textAlign: 'right', width: '70%' }}
+                        />
+                        <label
+                            htmlFor='email'
+                            className='login-label'
+                            style={{ textAlign: 'right' }}
+                        >
+                            Longitude
+                        </label>
+                        <input
+                            type='email'
+                            name='long'
+                            value={long}
+                            placeholder='e.g., -122.176'
+                            onChange={onChange}
+                            required
+                            style={{ marginLeft: 'auto', textAlign: 'right', width: '70%' }}
+                        />
+                        <form onSubmit={onSubmit}>
+                            <a href='#'>
+                                <input
+                                type='submit'
+                                value='Set Destination'
+                                className='btn btn-primary btn-block'
+                                style={{ marginLeft: 'auto', width: '8vw' }}
+                                />
+                            </a>
+                        </form>
+                    </div>
+                    <div style={{ position: 'relative', width: '32rem', height: '25rem' }}>
+                        <Map
+                            google={google}
+                            zoom={8}
+                            style={mapStyles}
+                            initialCenter={{ lat: latitude, lng: longitude }}
+                            id='map'
+                        />
+                        <Marker
+                            position={curr}
+                            name={'curr'}
+                            map={document.getElementById('map')}
+                        />
+                        <Marker 
+                            position={dest}
+                            name={'dest'}
+                            map={document.getElementById('map')}
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className='grid-hor'>
+                <div style={{ left: '-20rem' }}>
+                    <h1 className='text-primary' style={{ fontSize: '3rem', fontWeight: 'bolder' }}>Hazard Rating</h1>
+                    <body style={{ fontSize: '2rem' }}>{hazardRating}</body>
+                </div>
+                <div>
+                    <h1 className='text-primary' style={{ fontSize: '3rem', fontWeight: 'bolder' }}>Shortest Path</h1>
+                    <body>{path}</body>
+                </div>
+            </div>
         </div>
     );
 }
 
-export default Main;
+Main.propTypes = {
+    carModel: PropTypes.string.isRequired,
+    hazardRating: PropTypes.number,
+    path: PropTypes.string
+};
+
+Main.defaultProps = {
+    carModel: 'Volkswagen Beetle',
+    hazardRating: 0,
+    path: ''
+};
+
+
+export default geolocated({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    userDecisionTimeout: 5000
+})(GoogleApiWrapper({
+    apiKey: 'AIzaSyCdN-PaaKG8Bh5xOhQLpWmAuGNX_0e3b2g'
+})(Main));
